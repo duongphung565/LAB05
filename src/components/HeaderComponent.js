777b47b9655646d32ad3fca4c087c0f71fec61e8
duongphung565 <nguyenphungduong565@gmail.com>
2022-07-16 23:17:11 +0700

    lab05

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,90 @@
+
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+
+    }
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar className="navbar-dark bg-primary" expand="md" >
+                    <div className='container'>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.setState.isNavOpen} navbar >
+
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg">
+
+
+                                        </span>
+                                        Home
+
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg">
+
+
+                                        </span>
+                                        About Us
+
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg">
+
+
+                                        </span>
+                                        Menu
+
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg">
+
+
+                                        </span>
+                                        Constact Us
+
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+                <Jumbotron>
+                    <div className='container'>
+                        <div className="row row-header">
+                            <div className="col-12 col-sm-6">
+                                <h1>Ristorante Con Fusion</h1>
+                                <p>Font Awesome is a full suite of 675 pictographic icons for easy scalable vector graphics on websites, created and maintained by Dave Gandy. Stay up to date with the latest release and announcements on Twitter</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </Jumbotron>
+            </React.Fragment>
+        );
+    }
+}
+export default Header;
